01dbf5680d469e01f35b7b0697743a826afba527
Lokesh Mittal <lokesh.adonis@gmail.com>
2024-11-21 19:03:36 -0500

    Added Microservice - accounts, cards, loan

--- accounts/src/main/java/com/lokesh/accounts/repository/AccountsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lokesh.accounts.repository;
-
-import com.lokesh.accounts.entity.Accounts;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccountsRepository extends JpaRepository<Accounts, Long> {
-}
--- accounts/src/main/java/com/lokesh/accounts/service/IAccountsService.java
+++
@@ -1,7 +0,0 @@
-package com.lokesh.accounts.service;
-
-import com.lokesh.accounts.dto.CustomerDto;
-
-public interface IAccountsService {
-    void createAccount(CustomerDto customerDto);
-}
